29275fdb8a2efead252cbd201fd4a979d94abef9
lukeprime <108359791+lukeprime@users.noreply.github.com>
2024-03-11 09:58:21 -0500

    Make crouch hold an option (#3)

--- src/LethalCompanyPatchedPlugin.cs
+++ src/LethalCompanyPatchedPlugin.cs
@@ -19,6 +19,7 @@ namespace OoLunar.LethalCompanyPatched
         internal static ConfigEntry<float> Slipperiness = null!;
         internal static ConfigEntry<bool> InstantJump = null!;
         internal static ConfigEntry<bool> ShowHudPercentages = null!;
+        internal static ConfigEntry<bool> CrouchHold = null!;
 
         [SuppressMessage("Roslyn", "IDE0051", Justification = "Unity will call this method through reflection. Should've been an interface method but w/e.")]
         private void Awake()
@@ -27,6 +28,7 @@ namespace OoLunar.LethalCompanyPatched
             Logger.LogDebug($"{MyPluginInfo.PLUGIN_NAME} started loading!");
 
             StaticLogger = Logger;
+            CrouchHold = Config.Bind("General", "crouchHold", true, "Enable/disable crouch hold. If disabled, crouch functions as a toggle with the additional behavior of going back into a crouch upon landing from a crouch jump.");
             InstantJump = Config.Bind("General", "instantJump", true, "Enable/disable instant jump. Removes the delay with jumping when enabled.");
             InstantSprint = Config.Bind("General", "instantSprint", 2.25f, "How fast to accelerate to sprint value of 2.25. 2.25 is the max, so it's instant acceleration.");
             Slipperiness = Config.Bind("General", "slipperiness", 10f, "The amount of slipperiness when running and changing direction. 10-15f is a good value for little to no slippery feeling.");
